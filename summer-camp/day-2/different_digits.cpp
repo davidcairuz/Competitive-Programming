2a8647287e668a6c8d0cb88972988b22464cbd80
David Cairuz <davidcairuz@gmail.com>
2019-01-27 20:48:21 -0200

    Create different_digits.cpp
    Solution for ICMC ICPC problem called: Different Digits

---
+++ summer-camp/day-2/different_digits.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define inf 0x3f3f3f3f
+
+int repeated(int n) {
+    set<int> digits;
+    while(n > 0) {
+        int digit = n % 10;
+        if(digits.find(digit) != digits.end()) return false;
+
+        digits.insert(digit);
+        n /= 10;
+    }  
+
+    return true;
+} 
+
+int main() {
+    int n, m;
+    
+    while(cin >> n >> m) {
+
+        int ans = 0;
+        for(int i = n; i <= m; i++) {
+            ans += repeated(i);
+        }
+
+        cout << ans << endl;
+    }
+}

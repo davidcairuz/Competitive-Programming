457d5e898036fd95658b03db5027622ab99167a4
David Cairuz <davidcairuz@gmail.com>
2019-02-09 22:55:39 -0300

    Create database_of_clients.py

---
+++ solutions/brazilian-summer-camp-2019/day-01/database_of_clients.py
@@ -0,0 +1,16 @@
+n = int(input())
+emails = set()
+
+for i in range(n):
+	email = input()
+	user, provider = email.split('@')
+
+	if('+' in user):
+		user, dump = email.split('+')
+
+	user = user.replace('.', '')
+	email = user + '@' + provider
+
+	emails.add(email)
+
+print(len(emails))
